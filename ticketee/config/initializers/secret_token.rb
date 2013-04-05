b3b883e29a9592fcd61b06b1fb8590ab95847bda
John Higginson <jhigginson@gmail.com>
2013-04-04 20:35:46 -0700

    created Ticketee

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '4a4f605732b5deb523324495b0c41a68939a87bfe88a0bc3f427334df22d5863343b52f445e8d5639c809dfc3c66db40375ecadbdc8737ba9afa3aace7830271'
