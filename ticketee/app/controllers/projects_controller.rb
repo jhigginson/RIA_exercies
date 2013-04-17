6602b60820c87cbb30a03788c92e50002ed4e26f
John Higginson <jhigginson@gmail.com>
2013-04-16 22:43:07 -0700

    Implement delete funcitonality for projects

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -12,29 +12,36 @@ class ProjectsController < ApplicationController
     if @project.save
      flash[:notice] = "Project has been created."
      redirect_to @project
-   else
+    else
      flash[:alert] = "Project has not been created."
      render :action => "new"
-   end
- end
-
- def show
-   @project = Project.find(params[:id])
- end
-
- def edit
-  @project = Project.find(params[:id])
-end
-
-def update
-  @project = Project.find(params[:id])
-  if @project.update_attributes(params[:project])
-    flash[:notice] = "Project has been updated."
-    redirect_to @project
-  else
-    flash[:alert] = "Project has not been updated."
-    render :action => "edit"
+    end
   end
-end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
 
 end
