ecb5a5aa8cb3fe5bc5daf1e6b5ebf182d73707ae
John Higginson <jhigginson@gmail.com>
2013-04-16 22:30:27 -0700

    Add updating projects functionality

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,24 +1,40 @@
 class ProjectsController < ApplicationController
 	def index
     @projects = Project.all
-	end
-
-	def new
-		@project = Project.new
-	end
-
-	def create
-		@project = Project.new(params[:project])
-		if @project.save
-			flash[:notice] = "Project has been created."
-			redirect_to @project
-		else
-    	flash[:alert] = "Project has not been created."
-    	render :action => "new"
-  	end
   end
 
-  def show
-  	@project = Project.find(params[:id])
+  def new
+    @project = Project.new
   end
+
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+     flash[:notice] = "Project has been created."
+     redirect_to @project
+   else
+     flash[:alert] = "Project has not been created."
+     render :action => "new"
+   end
+ end
+
+ def show
+   @project = Project.find(params[:id])
+ end
+
+ def edit
+  @project = Project.find(params[:id])
+end
+
+def update
+  @project = Project.find(params[:id])
+  if @project.update_attributes(params[:project])
+    flash[:notice] = "Project has been updated."
+    redirect_to @project
+  else
+    flash[:alert] = "Project has not been updated."
+    render :action => "edit"
+  end
+end
+
 end
