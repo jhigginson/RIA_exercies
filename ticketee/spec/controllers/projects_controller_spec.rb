7dbe69626d509cb23a20c5fb1008ccda2959a8de
John Higginson <jhigginson@gmail.com>
2013-05-14 17:57:53 -0700

    Make projects only visible to users with permission to see them

--- ticketee/spec/controllers/projects_controller_spec.rb
+++ ticketee/spec/controllers/projects_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ProjectsController do
 
   let(:user) { Factory(:confirmed_user) }
-  let(:project) { mock_model(Project, id: 1) }
+  let(:project) { Factory(:project) }
 
   context "standard users" do
     before do
@@ -15,20 +15,28 @@ describe ProjectsController do
       :edit => :get,
       :update => :put,
       :destroy => :delete }.each do |action, method|
-        it "cannot access the #{action} action" do
-          sign_in(:user, user)
-          send(method, action, :id => project.id)
-          response.should redirect_to(root_path)
-          flash[:alert].should eql("You must be an admin to do that.")
-        end
+      it "cannot access the #{action} action" do
+        sign_in(:user, user)
+        send(method, action, :id => project.id)
+        response.should redirect_to(root_path)
+        flash[:alert].should eql("You must be an admin to do that.")
       end
-
     end
 
-    it "displays an error for a missing project" do
-      get :show, :id => "not-here"
+    it "cannot access the show action without permission" do
+      sign_in(:user, user)
+      get :show, :id => project.id
       response.should redirect_to(projects_path)
-      message = "The project you were looking for could not be found."
-      flash[:alert].should == message
+      flash[:alert].should eql("The project you were looking " +
+                               "for could not be found.")
     end
   end
+
+  it "displays an error for a missing project" do
+    sign_in(:user, user)
+    get :show, :id => "not-here"
+    response.should redirect_to(projects_path)
+    message = "The project you were looking for could not be found."
+    flash[:alert].should == message
+  end
+end
