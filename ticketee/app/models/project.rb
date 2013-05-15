d934c99019412eb5800a6c4093f054a3d6dd8b6a
John Higginson <jhigginson@gmail.com>
2013-05-14 20:11:37 -0700

    Don't show projects that a user doesn't have permission to see

--- ticketee/app/models/project.rb
+++ ticketee/app/models/project.rb
@@ -3,10 +3,15 @@ class Project < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
   has_many :permissions, as: :thing
+
   def self.viewable_by(user)
     joins(:permissions).where(permissions: { action: "view", user_id: user.id })
   end
 
   has_many :tickets, :dependent => :delete_all
+
+  def self.for(user)
+    user.admin? ? Project : Project.viewable_by(user)
+  end
 
 end
